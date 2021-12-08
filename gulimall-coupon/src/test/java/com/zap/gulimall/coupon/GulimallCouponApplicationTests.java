c2fa76c1cab7c794dae0676c3a8c59140583abc4
evan_glb <evan@qq.com>
2021-12-08 16:42:21 +0800

    谷粒商城的初始化提交

---
+++ gulimall-coupon/src/test/java/com/zap/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zap.gulimall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
